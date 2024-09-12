8b213f130341df7282b2e63a76e65c43c7ba2b0e
Vladyslav D <donetsvlad444@gmail.com>
2024-09-12 22:35:12 +0300

    remove loader, updated more btn

--- src/app/[locale]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Loader from "@/components/Loader";
-import { useLocale } from "next-intl";
-import { unstable_setRequestLocale } from "next-intl/server";
-
-export default function Loading() {
-  const locale = useLocale();
-  unstable_setRequestLocale(locale);
-  return <Loader />;
-}
